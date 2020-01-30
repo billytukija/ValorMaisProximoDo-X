e9abfd33d9cd6a5ef5011302c25928a793135e8a
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-01-29 22:59:13 -0300

    Função para localizar o valor mais próximo do outro (double)

---
+++ ClosetBelow/ClosetBelow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClosetBelow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<Double> { 1.02, 0.9, 1.04, 0.8 };
+            var pivot = 1;
+            var closestBelow = Convert.ToDouble(pivot) - numbers.Where(n => n <= pivot).Min(n => pivot - n);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
